c06d5a4a79b1379b0f48e23e0ce8fdd11e2dcf89
isaacwu666 <isaacwu@live.com>
2020-07-04 10:37:00 +0800

    Initial commit

---
+++ src/main/java/tomweb/xyz/bjcms/BjcmsApplication.java
@@ -0,0 +1,13 @@
+package tomweb.xyz.bjcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjcmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BjcmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tomweb/xyz/bjcms/BjcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package tomweb.xyz.bjcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjcmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
