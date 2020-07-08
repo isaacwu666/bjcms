fec6de52c01ad66dea42aca20c9a9796841c4002
isaacwu <isaacwu@live.com>
2020-07-08 21:52:36 +0800

    ba

---
+++ src/main/java/tomweb/xyz/bjcms/utils/BaiJiaHaoUtils.java
@@ -0,0 +1,14 @@
+package tomweb.xyz.bjcms.utils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 百家号工具类
+ */
+@Component
+public class BaiJiaHaoUtils {
+
+
+
+
+}
---
+++ start.md
@@ -0,0 +1,3 @@
+创建数据：
+`CREATE USER 'bj_cms'@'%' IDENTIFIED BY 'Bj_cms.123';
+ GRANT USAGE ON *.* TO 'bj_cms'@'%' IDENTIFIED BY 'Bj_cms.123';
